e6801632c3f8a2db898329ed1bfbaf1ce8af1363
dev-DTECH <debdeep.dt@gmail.com>
2023-05-25 20:26:50 +0530

    fix: SequelizeConnectionError: self signed certificate

--- utils/db.js
+++ utils/db.js
@@ -6,6 +6,10 @@ const {Sequelize} = require("sequelize");
 
 const sequelize = new Sequelize(process.env.CONFIG_DB+(process.env.CONFIG_SSL === "true"? "?ssl=true": ""), {
     logging: process.env.CONFIG_DEBUG === 'true',
+    ssl:{
+        require: true,
+        rejectUnauthorized: false
+    }
 })
 
 module.exports = sequelize
