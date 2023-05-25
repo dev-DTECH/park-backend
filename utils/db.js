0f4455dd9549d41ce42270ee2199cec299cb02d1
dev-DTECH <debdeep.dt@gmail.com>
2023-05-25 20:22:16 +0530

    cleanup

--- utils/db.js
+++ utils/db.js
@@ -4,12 +4,8 @@ const {Sequelize} = require("sequelize");
 
 // const sequelize = new Sequelize('sqlite::memory:');
 
-const sequelize = new Sequelize(process.env.CONFIG_DB+(process.env.CONFIG_DB === 'true'? "?ssl=true": ""), {
+const sequelize = new Sequelize(process.env.CONFIG_DB+(process.env.CONFIG_SSL === "true"? "?ssl=true": ""), {
     logging: process.env.CONFIG_DEBUG === 'true',
-    ssl:{
-        require: true,
-        rejectUnauthorized: false
-    }
 })
 
 module.exports = sequelize
