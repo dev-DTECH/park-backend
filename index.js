45b4f0a9475e198002a81d8c2a50aa6087ea3bad
dev-DTECH <debdeep.dt@gmail.com>
2023-05-25 15:41:51 +0530

    consistent db

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const app= express();
 const sequelize = require('./utils/db')
 const {ParkingSpotModel} = require("./models/parkingSpot");
 sequelize.sync(
-    {force:true}
+    // {force:true}
 )
 .then(async result => {
-    // console.log(result)
-    for (let i = 0; i < 100; i++) {
-        await ParkingSpotModel.create({
-            name: `parking spot name ${i}`,
-            address: `parking spot address ${i}`
-        })
-    }
+    // for (let i = 0; i < 100; i++) {
+    //     await ParkingSpotModel.create({
+    //         name: `parking spot name ${i}`,
+    //         address: `parking spot address ${i}`
+    //     })
+    // }
+    console.log("DB Connected!")
 })
 // .catch(err => console.log(err))
 
--- utils/db.js
+++ utils/db.js
@@ -1,12 +1,11 @@
+require('dotenv').config()
 
 const {Sequelize} = require("sequelize");
 
 // const sequelize = new Sequelize('sqlite::memory:');
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: '../db.sqlite3',
-    logging: false
+const sequelize = new Sequelize(process.env.CONFIG_DB+'?ssl=true', {
+    logging: process.env.CONFIG_DEBUG === 'true'
 })
 
 module.exports = sequelize
